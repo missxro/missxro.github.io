66cc1719ab30ed6c4c1fd0a1fb040fabf9cd72a7
missxro <roplay112@gmail.com>
2026-05-24 20:08:04 +0200

    Remove compiled files from repo after migrating to GitHub Actions

---
+++ src/config.ts
@@ -0,0 +1,65 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://missxro.github.io/",
+  author: "MissxRo",
+  profile: "https://github.com/missxro",
+  desc: "Blog personal dedicado a la informática.",
+  title: "xRo's Blog",
+  ogImage: "logo.png", // Pendiente para cambiar
+  lightAndDarkMode: true,
+  postPerIndex: 4, // Número de posts que se muestran en "recientes"
+  postPerPage: 3, // Posts que se muestran por cada página
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "es",
+  langTag: ["es-ES"],
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: true,
+  svg: true,
+  width: 108,
+  height:23,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/MissxR0",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/xrro.sh/",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/rocio-del-pilar-felipe-maraver/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: false,
+  },
+  {
+    name: "Mail",
+    href: "mailto:roplay112@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "Twitch",
+    href: "https://www.twitch.tv/xrro_",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: true,
+  },
+  {
+    name: "Discord",
+    href: "https://discordapp.com/users/294874107177205761",
+    linkTitle: `${SITE.title} on Discord`,
+    active: true,
+  }
+];
---
+++ src/pages/whoami.md
@@ -0,0 +1,14 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "¿Quién soy?"
+---
+
+Soy xRo, aunque mi nombre real es Rocío. Soy nueva y no tengo mucha experiencia, pero me gustaría poder ayudar a otras personas que, como a mi, les gusta romper, cacharrear y sobretodo, aprender.<br><br>
+En mi blog puedes encontrar contenido relacionado con la administración de sistemas operativos (<strong class="important-text">en especial linux :D</strong>), la ciberseguridad, hacking ético, redes...
+
+## Contacto
+
+Quien quiera escribirme puede hacerlo sin ningún tipo de problema, ¡estoy abierta a todo! (Incluso si buscas +1 para completar el stack de valorant xD)
+
+- [Discord](https://discordapp.com/users/294874107177205761)
+- [Email](mailto:roplay112@gmail.com)
